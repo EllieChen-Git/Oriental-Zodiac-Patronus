e6da613ca7c65c39775a9d2e1281a5b05ed59f45
EllieChen-Git <elliechen.etc@gmail.com>
2019-09-03 10:17:08 +1000

    DRY - birth animals in case statment

--- index.rb
+++ index.rb
@@ -1,3 +1,5 @@
+birth_animals = ["pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog"]
+
 # puts "Hi there, how are you? What\'s your name?"
 # name = gets.chomp
 # puts "Hello, #{name}! Wanna know more about your Chinese zodiac animal?"
@@ -8,34 +10,32 @@ birth_year = gets.chomp.to_i
 age = 2019 - birth_year 
 puts "You were born in #{birth_year} so you are #{age} years old this year!"
 
-birth_animals = ["pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog"]
-
 index = age % 12
-
-if index == 0
-    puts "your birth animal is #{birth_animals[0]}"
-elsif index == 1
-    puts "your birth animal is #{birth_animals[1]}"
-elsif index == 2
-    puts "your birth animal is #{birth_animals[2]}"
-elsif index == 3
-    puts "your birth animal is #{birth_animals[3]}"
-elsif index == 4
-    puts "your birth animal is #{birth_animals[4]}"
-elsif index == 5
-    puts "your birth animal is #{birth_animals[5]}"
-elsif index == 6
-    puts "your birth animal is #{birth_animals[6]}"
-elsif index == 7
-    puts "your birth animal is #{birth_animals[7]}"
-elsif index == 8
-    puts "your birth animal is #{birth_animals[8]}"
-elsif index == 9
-    puts "your birth animal is #{birth_animals[9]}"
-elsif index == 10
-    puts "your birth animal is #{birth_animals[10]}"
+case index
+when 0
+    puts "Your birth animal is... #{birth_animals[0]}!"
+when 1
+    puts "Your birth animal is... #{birth_animals[1]}!"
+when 2
+    puts "Your birth animal is... #{birth_animals[2]}!"
+when 3
+    puts "Your birth animal is... #{birth_animals[3]}!"
+when 4
+    puts "Your birth animal is... #{birth_animals[4]}!"
+when 5
+    puts "Your birth animal is... #{birth_animals[5]}!"
+when 6
+    puts "Your birth animal is... #{birth_animals[6]}!"
+when 7
+    puts "Your birth animal is... #{birth_animals[7]}!"
+when 8
+    puts "Your birth animal is... #{birth_animals[8]}!"
+when 9
+    puts "Your birth animal is... #{birth_animals[9]}!"
+when 10
+    puts "Your birth animal is... #{birth_animals[10]}!"
 else
-    puts "your birth animal is #{birth_animals[11]}"
+    puts "Your birth animal is... #{birth_animals[11]}!"
 end
 
 
