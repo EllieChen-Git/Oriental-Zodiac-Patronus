408d599cfd37beb8cbfa6873a2fb72b99675d21e
EllieChen-Git <elliechen.etc@gmail.com>
2019-09-03 13:03:28 +1000

    DRY - best matches

--- index.rb
+++ index.rb
@@ -1,47 +1,24 @@
 birth_animals = ["monkey", "rooster", "dog", "pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat"]
+# birth_animals = ["monkey0", "rooster1", "dog2", "pig3", "rat4", "ox5", "tiger6", "rabbit7", "dragon8", "snake9", "horse10", "goat11"]
 
-puts "Hi there, how are you? What\'s your name?"
-name = gets.chomp
-puts "Hello, #{name}! Wanna know more about your Chinese zodiac animal?"
-puts "Let\'s start with your birth year!"
-puts
+# puts "Hi there, how are you? What\'s your name?"
+# name = gets.chomp
+# puts "Hello, #{name}! Wanna know more about your Chinese zodiac animal?"
+# puts "Let\'s start with your birth year!"
+# puts
 puts "In What year were you born?"
 birth_year = gets.chomp.to_i
-age = 2019 - birth_year 
-puts "You were born in #{birth_year} so you are #{age} years old this year!"
+# age = 2019 - birth_year 
+# puts "You were born in #{birth_year} so you are #{age} years old this year!"
 index = (birth_year % 12)
 puts "Your birth animal is... #{birth_animals[index]}!"
 
+index_2 = index + 4
+if index_2 >= 11
+    index_2 = index_2 -12
+end
+index_3 = index_2 + 4
 puts "Now you know what your birth animal is..."
 puts "Enter your birth animal here to know what birth animals are your best matches!"
-your_birth_animal = gets.chomp.downcase
-puts "As a #{your_birth_animal}, your best matchese are #{birth_animals[index]}, #{birth_animals[index + 4]}, #{birth_animals[index + 8]}."
-
-
-
-# case your_birth_animal
-# when "#{birth_animals[0]}"
-#     puts "Your best mataches are #{birth_animals[0]}, #{birth_animals[4]} and #{birth_animals[8]}!"
-# when "#{birth_animals[1]}"
-#     puts "Your best mataches are #{birth_animals[1]}, #{birth_animals[5]} and #{birth_animals[9]}!"
-# when "#{birth_animals[2]}"
-#     puts "Your best mataches are #{birth_animals[2]}, #{birth_animals[6]} and #{birth_animals[10]}!"
-# when "#{birth_animals[3]}"
-#     puts "Your best mataches are #{birth_animals[3]}, #{birth_animals[7]} and #{birth_animals[11]}!"
-# when "#{birth_animals[4]}"
-#     puts "Your best mataches are #{birth_animals[4]}, #{birth_animals[8]} and #{birth_animals[0]}!"
-# when "#{birth_animals[5]}"
-#     puts "Your best mataches are #{birth_animals[5]}, #{birth_animals[9]} and #{birth_animals[1]}!"
-# when "#{birth_animals[6]}"
-#     puts "Your best mataches are #{birth_animals[6]}, #{birth_animals[10]} and #{birth_animals[2]}!"
-# when "#{birth_animals[7]}"
-#     puts "Your best mataches are #{birth_animals[7]}, #{birth_animals[11]} and #{birth_animals[3]}!"
-# when "#{birth_animals[8]}"
-#     puts "Your best mataches are #{birth_animals[8]}, #{birth_animals[0]} and #{birth_animals[4]}!"
-# when "#{birth_animals[9]}"
-#     puts "Your best mataches are #{birth_animals[9]}, #{birth_animals[1]} and #{birth_animals[5]}!"
-# when "#{birth_animals[10]}"
-#     puts "Your best mataches are #{birth_animals[10]}, #{birth_animals[2]} and #{birth_animals[6]}!"
-# else
-#     puts "Your best mataches are #{birth_animals[11]}, #{birth_animals[3]} and #{birth_animals[7]}!"
-# end
+your_birth_animal = gets.chomp
+puts "As a #{your_birth_animal}, your best matchese are #{birth_animals[index]}, #{birth_animals[index_2]}, #{birth_animals[index_3]}."
