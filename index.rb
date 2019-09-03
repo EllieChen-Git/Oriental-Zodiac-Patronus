cc63a66b268acac23092807e392f89671b5c8e70
EllieChen-Git <elliechen.etc@gmail.com>
2019-09-03 13:20:40 +1000

    added no match

--- index.rb
+++ index.rb
@@ -1,24 +1,34 @@
-birth_animals = ["monkey", "rooster", "dog", "pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat"]
 # birth_animals = ["monkey0", "rooster1", "dog2", "pig3", "rat4", "ox5", "tiger6", "rabbit7", "dragon8", "snake9", "horse10", "goat11"]
 
-# puts "Hi there, how are you? What\'s your name?"
-# name = gets.chomp
-# puts "Hello, #{name}! Wanna know more about your Chinese zodiac animal?"
-# puts "Let\'s start with your birth year!"
-# puts
+birth_animals = ["monkey", "rooster", "dog", "pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat"]
+
+puts "Hi there, how are you? What\'s your name?"
+name = gets.chomp
+puts "Hello, #{name}! Wanna know more about your Chinese zodiac animal?"
+puts "Let\'s start with your birth year!"
+puts
 puts "In What year were you born?"
 birth_year = gets.chomp.to_i
-# age = 2019 - birth_year 
-# puts "You were born in #{birth_year} so you are #{age} years old this year!"
+age = 2019 - birth_year 
+puts "You were born in #{birth_year}, so you are #{age} years old this year!"
 index = (birth_year % 12)
-puts "Your birth animal is... #{birth_animals[index]}!"
-
+puts "And your birth animal is... #{birth_animals[index]}!"
+puts
 index_2 = index + 4
-if index_2 >= 11
+if index_2 > 10
     index_2 = index_2 -12
 end
 index_3 = index_2 + 4
-puts "Now you know what your birth animal is..."
+puts "Now you know what your birth animal is... Let\'s Find out your zodiac animals compatibility!"
 puts "Enter your birth animal here to know what birth animals are your best matches!"
 your_birth_animal = gets.chomp
 puts "As a #{your_birth_animal}, your best matchese are #{birth_animals[index]}, #{birth_animals[index_2]}, #{birth_animals[index_3]}."
+puts
+index_4 = index + 6
+if index_4 > 10
+    index_4 = index_4 -12
+end
+
+puts "You can also enter your birth animal here to know what birth animal has a direct conflict with you!"
+your_birth_animal = gets.chomp
+puts "Oh, no! As a #{your_birth_animal}, you don\'t get along with #{birth_animals[index_4]}."
